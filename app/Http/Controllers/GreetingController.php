1eae649ac10a8214a1d399ebe567586cda6aff8f
shimamatz@gmail.com <shimamatz@gmail.com>
2021-12-31 05:44:22 +0900

    テスト：リクエストデータのチェック

--- app/Http/Controllers/GreetingController.php
+++ app/Http/Controllers/GreetingController.php
@@ -11,6 +11,12 @@ class GreetingController extends Controller
     }
 
     public function welcome(Request $request){
+        $rule = [
+            'username' => 'required|max:20'
+        ];
+
+        $request->validate($rule);
+
         $username = $request->input('username');
         return view('greeting.welcome', ['username' => $username]);
     }

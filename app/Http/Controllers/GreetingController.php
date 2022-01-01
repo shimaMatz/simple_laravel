38e9cd974aa41aba0293439bc5be7f4d28e043e6
shimamatz@gmail.com <shimamatz@gmail.com>
2022-01-02 04:25:07 +0900

    テスト：セッションデータの保持

--- app/Http/Controllers/GreetingController.php
+++ app/Http/Controllers/GreetingController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests\GreetingRequest;
+
 class GreetingController extends Controller
 {
     public function index(){
